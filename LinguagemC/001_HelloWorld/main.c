5e2fc42d27574b19d3405b5ec491ea4f52504287
Carlos Nascimento <cnbatalha@gmail.com>
2016-06-06 13:38:18 -0400

    - adicionando projeto Hello World - adicionando projetp Variaveis

---
+++ LinguagemC/001_HelloWorld/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+
+	printf("MENU \n"); 
+	printf("1\t PRATO SIMPLE \n");
+	printf("2\t PRATO COMPLETO \n"); 
+	printf("0\t SAIR\n"); 
+		
+}
---
+++ LinguagemC/002_Variaveis/variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+main()
+{
+	float nota_final = 10, Nota_final; 
+	
+	int p1onto1;
+	// boolean valor;
+	char nome;
+	
+	printf( "Valor de nota_final é %f \n", nota_final);
+	printf( "Valor de p1onto1 é %d \n", p1onto1);
+	
+}
